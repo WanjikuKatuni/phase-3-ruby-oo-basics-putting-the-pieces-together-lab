a456c1c79b7a3c4ae8849e6cb160622c586ea914
wanjiku <annritawanjiku77@gmail.com>
2022-10-26 13:51:25 +0300

    completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,38 @@
+require 'pry'
 
+class Book
+
+    attr_reader :title
+
+    attr_accessor :author, :page_count, :genre
+ 
+
+    def initialize(title)
+        @title = title
+      
+        
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+
+    
+end
+
+book_info = Book.new("And The There Were None")
+
+book_info. author = "Agatha Christie"
+book_info.page_count = 272
+book_info.genre ="Mystery"
+
+
+puts book_info.title
+
+
+
+
+
+# binding.pry
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,35 @@
 # Make your shoe class here!
+
+require 'pry'
+
+class Shoe 
+    attr_reader :brand
+
+    attr_accessor :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+end
+
+
+
+
+shoe_brand = Shoe.new("Adidas")
+
+
+shoe_brand.color = "red"
+shoe_brand.size = 9.5
+shoe_brand.material ="suede"
+shoe_brand.condition = "tattered"
+
+
+puts shoe_brand.brand
+puts shoe_brand.color
+
+# binding.pry
